b521174ba08170aae916cc6ce1a3395735b32b18
john <zxcdezjxx@163.com>
2016-12-11 04:29:12 -0800

    first commit

---
+++ bin/classes.dex.d
@@ -0,0 +1,2 @@
+/home/john/workspace/testjar/bin/classes.dex : \
+/home/john/workspace/testjar/bin/classes/com/john/testcases.class \
---
+++ src/com/john/testcases.java
@@ -0,0 +1,20 @@
+package com.john;
+
+import com.android.uiautomator.core.UiDevice;
+import com.android.uiautomator.core.UiObject;
+
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class testcases extends UiAutomatorTestCase {
+
+	UiDevice device = UiDevice.getInstance();
+	public void testQQ() throws Exception{
+		device.pressHome();
+		UiObject qq = new UiObject(new UiSelector().text("QQ"));
+		qq.clickAndWaitForNewWindow();
+		device.pressBack();
+		device.pressRecentApps();
+		device.pressBack();
+	}
+}
